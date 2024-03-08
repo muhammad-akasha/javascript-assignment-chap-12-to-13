598d807a8b95da09a02788fb18a74c3aa5012141
Muhammad Akasha <153507162+muhammad-akasha@users.noreply.github.com>
2024-03-08 18:04:56 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,111 +1,111 @@
-// //  assignment chap 12 to 13.
+//  assignment chap 12 to 13.
 
-// // Question no 1.
+// Question no 1.
 
-// let Capital = prompt("Enter a character:");
+let Capital = prompt("Enter a character:");
 
-// let charCode = Capital.charCodeAt(0);
+let charCode = Capital.charCodeAt(0);
 
-// if (charCode >= 65 && charCode <= 90 ){
-//     console.log("It is capital alphabet." , Capital);
+if (charCode >= 65 && charCode <= 90 ){
+    console.log("It is capital alphabet." , Capital);
 
-// } else if (charCode >= 97 && charCode <= 122) {
-//  console.log("it is small alphabet" , Capital)
-// }
-//  else {
-//     console.log("It is number or something." , Capital );
-// }
+} else if (charCode >= 97 && charCode <= 122) {
+ console.log("it is small alphabet" , Capital)
+}
+ else {
+    console.log("It is number or something." , Capital );
+}
 
 
 
-// // Question no 2
+// Question no 2
 
-// let number_1 = +prompt("enter a number");
+let number_1 = +prompt("enter a number");
 
-// let number_2 = +prompt("enter a number");
+let number_2 = +prompt("enter a number");
 
-// if (number_1 > number_2){
+if (number_1 > number_2){
 
-//     console.log(number_1 , "is Larger then" , number_2);
+    console.log(number_1 , "is Larger then" , number_2);
 
-// }
+}
 
-// else if (number_1 === number_2){
+else if (number_1 === number_2){
 
-//     console.log(number_1 , " is Equal to " , number_2);
+    console.log(number_1 , " is Equal to " , number_2);
 
-// }
-//  else if (number_2 > number_1){
+}
+ else if (number_2 > number_1){
 
-//     console.log(number_2 , "is Larger then" , number_1);
+    console.log(number_2 , "is Larger then" , number_1);
     
-// }
+}
 
-// else if (number_2 === number_1){
+else if (number_2 === number_1){
 
-//     console.log(number_2 , " is Equal to " , number_1);
-// }
+    console.log(number_2 , " is Equal to " , number_1);
+}
 
-// // Question 3
+// Question 3
 
-// let compareNum = +prompt ("Enter A Number I Will Check Number Is Positive Negative Or Zero");
+let compareNum = +prompt ("Enter A Number I Will Check Number Is Positive Negative Or Zero");
 
-// if (compareNum === 0 ){
-//     console.log("Number is Zero");
-// }
-// else if (compareNum > 0 ){
-//     console.log("Number is Positive");
-// }
-// else if (compareNum < 0 ){
-//     console.log("Number is Negative ");
-// }
-
-
-// // Question 4 
-
-// let checkVovel = prompt ("enter any one aplabet");
-
-// if (checkVovel === "a"){
-
-//     console.log("Alphabet is Vovel");
-// }
-// else if (checkVovel === "e"){
-
-//     console.log("Alphabet is Vovel");
-// }
-// else if (checkVovel === "i"){
-
-//     console.log("Alphabet is Vovel");
-// }
-// else if (checkVovel === "o"){
-
-//     console.log("Alphabet is Vovel");
-// }
-// else if (checkVovel === "u"){
-
-//     console.log("Alphabet is Vovel");
-
-// }else {
-//     console.log("Alphabet is Not Vovel");
-// }
-
-// // Queation 5.
-
-// let Password = prompt("Enter Your Password");
-
-// userPassword = Password
-
-// let confirmPassword = prompt("Confirm Your PassWord");
-
-// if ( userPassword === confirmPassword ) {
-//     console.log("Correct The Password You Entered Does Match The Orignal Password");
-// }
-// else if ( userPassword !== confirmPassword ) {
-//     console.log("Incorrect Password");
-// }
+if (compareNum === 0 ){
+    console.log("Number is Zero");
+}
+else if (compareNum > 0 ){
+    console.log("Number is Positive");
+}
+else if (compareNum < 0 ){
+    console.log("Number is Negative ");
+}
 
 
-// // Question 6 .
+// Question 4 
+
+let checkVovel = prompt ("enter any one aplabet");
+
+if (checkVovel === "a"){
+
+    console.log("Alphabet is Vovel");
+}
+else if (checkVovel === "e"){
+
+    console.log("Alphabet is Vovel");
+}
+else if (checkVovel === "i"){
+
+    console.log("Alphabet is Vovel");
+}
+else if (checkVovel === "o"){
+
+    console.log("Alphabet is Vovel");
+}
+else if (checkVovel === "u"){
+
+    console.log("Alphabet is Vovel");
+
+}else {
+    console.log("Alphabet is Not Vovel");
+}
+
+// Queation 5.
+
+let Password = prompt("Enter Your Password");
+
+userPassword = Password
+
+let confirmPassword = prompt("Confirm Your PassWord");
+
+if ( userPassword === confirmPassword ) {
+    console.log("Correct The Password You Entered Does Match The Orignal Password");
+}
+else if ( userPassword !== confirmPassword ) {
+    console.log("Incorrect Password");
+}
+
+
+// Question 6 .
 
 
 
